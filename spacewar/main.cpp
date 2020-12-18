3b0454c925b6414d4c8afa86fc28b12a52f32ca5
Kildyushkin_Dmitriy <kild-dima@yandex.ru>
2020-12-18 23:00:50 +0300

    initial SFML setup

---
+++ spacewar/main.cpp
@@ -0,0 +1,57 @@
+#include <chrono>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML window");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("images/image.png"))
+    {
+        return EXIT_FAILURE;
+    }
+    sf::Sprite sprite(texture);
+
+    sf::Font font;
+    if (!font.loadFromFile("fonts/arial.ttf"))
+    {
+        return EXIT_FAILURE;
+    }
+    sf::Text text("Hello SFML", font, 50);
+
+    // window.setFramerateLimit(60);
+
+    sf::Clock timer;
+    
+    while (window.isOpen())
+    {
+        float dt = timer.restart().asSeconds();
+        dt = std::min(dt, 0.1f);
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            sf::Vector2f pos = sprite.getPosition();
+            pos.x += 50.f * dt;
+            sprite.setPosition(pos);
+        }
+
+        window.clear();
+
+        window.draw(sprite);
+        window.draw(text);
+
+        window.display();
+    }
+    
+    return EXIT_SUCCESS;
+}
